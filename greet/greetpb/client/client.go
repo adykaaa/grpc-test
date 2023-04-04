aff2209b533bcaf9dd2044a400fc5a1a3f6b6b01
adykaaa <gosupalanta@gmail.com>
2023-04-04 08:55:32 +0200

    bidirectional streaming functions done

--- greet/greetpb/client/client.go
+++ greet/greetpb/client/client.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
+	"time"
 
 	"github.com/adykaaa/grpc-test/greet/greetpb"
 	"google.golang.org/grpc"
@@ -22,9 +24,141 @@ func DoUnary(c greetpb.GreetServiceClient) {
 		fmt.Printf("Could not send the request!")
 	}
 	log.Printf("repsonse from the server %v", resp.Result)
-
 }
 
+func DoServerStreaming(c greetpb.GreetServiceClient) {
+	fmt.Println("Starting server streaming RPC...")
+
+	req := &greetpb.GreetManyTimesRequest{
+		Greeting: &greetpb.Greeting{
+			FirstName: "Toth",
+			LastName:  "Adam",
+		},
+	}
+
+	stream, err := c.GreetManyTimes(context.Background(), req)
+	if err != nil {
+		log.Fatalf("error while calling GreetManyTimes RPC: %v", err)
+	}
+
+	for {
+		msg, err := stream.Recv()
+		if err == io.EOF {
+			//we've reached end of stream
+			break
+		}
+		if err != nil {
+			log.Fatalf("Error while reading stream %v", err)
+		}
+
+		log.Printf("Response: " + msg.GetResult())
+	}
+}
+
+func DoClientStreaming(c greetpb.GreetServiceClient) {
+	fmt.Println("Starting client streaming RPC...")
+
+	requests := []*greetpb.LongGreetRequest{
+		{
+			Greeting: &greetpb.Greeting{
+				FirstName: "Adam",
+			},
+		},
+		{
+			Greeting: &greetpb.Greeting{
+				FirstName: "asadsadsa",
+			},
+		},
+		{
+			Greeting: &greetpb.Greeting{
+				FirstName: "LOLOLOLOL",
+			},
+		},
+		{
+			Greeting: &greetpb.Greeting{
+				FirstName: "Madeupname",
+			},
+		},
+	}
+
+	stream, err := c.LongGreet(context.Background())
+	if err != nil {
+		log.Fatalf("error while calling Long Greet: %v", err)
+	}
+
+	for _, req := range requests {
+		fmt.Printf("Sending request %v \n", req)
+		stream.Send(req)
+		time.Sleep(100 * time.Millisecond)
+	}
+
+	resp, err := stream.CloseAndRecv()
+	if err != nil {
+		log.Fatalf("Error during close and recieve! %v", err)
+	}
+
+	fmt.Printf("server response after client stream: %v", resp)
+}
+
+func DoBiDiStreaming(c greetpb.GreetServiceClient) {
+	fmt.Println("Starting client streaming RPC...")
+	requests := []*greetpb.GreetEveryoneRequest{
+		{
+			Greeting: &greetpb.Greeting{
+				FirstName: "Adam",
+			},
+		},
+		{
+			Greeting: &greetpb.Greeting{
+				FirstName: "asadsadsa",
+			},
+		},
+		{
+			Greeting: &greetpb.Greeting{
+				FirstName: "LOLOLOLOL",
+			},
+		},
+		{
+			Greeting: &greetpb.Greeting{
+				FirstName: "Madeupname",
+			},
+		},
+	}
+
+	//we create a stream by invoking the client
+	stream, err := c.GreetEveryone(context.Background())
+	if err != nil {
+		log.Fatalf("Error while creating stream: %v", err)
+	}
+
+	waitch := make(chan struct{})
+	//we send bunch of messages to the server
+	go func() {
+		for _, req := range requests {
+			fmt.Printf("Sending message %v \n", req)
+			stream.Send(req)
+			time.Sleep(1 * time.Second)
+		}
+		stream.CloseSend()
+	}()
+	//we receive a bunch of messages from the server
+	go func() {
+		for {
+			resp, err := stream.Recv()
+			if err != nil {
+				if err == io.EOF {
+					break
+				}
+				log.Fatalf("Error while receving %v", err)
+				break
+			}
+			fmt.Printf("Received: %v \n", resp.GetResult())
+		}
+		close(waitch)
+	}()
+	//block until everything is done
+	<-waitch
+}
 func main() {
 	cc, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
 	if err != nil {
@@ -34,5 +168,8 @@ func main() {
 	defer cc.Close()
 
 	c := greetpb.NewGreetServiceClient(cc)
-	DoUnary(c)
+	//DoUnary(c)
+	//DoClientStreaming(c)
+	//DoServerStreaming(c)
+	DoBiDiStreaming(c)
 }
--- greet/greetpb/server/server.go
+++ greet/greetpb/server/server.go
@@ -3,8 +3,11 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
 	"net"
+	"strconv"
+	"time"
 
 	"github.com/adykaaa/grpc-test/greet/greetpb"
 	"google.golang.org/grpc"
@@ -14,10 +17,75 @@ type server struct {
 	greetpb.UnimplementedGreetServiceServer
 }
 
+// unary
 func (*server) Greet(ctx context.Context, req *greetpb.GreetRequest) (*greetpb.GreetResponse, error) {
-	fmt.Printf("greet function was invoked with %v", req)
-	firstName := req.Greeting.FirstName
-	return &greetpb.GreetResponse{Result: "Hello " + firstName}, nil
+	fmt.Printf("greet function was invoked with %v \n", req)
+	return &greetpb.GreetResponse{Result: "Hello " + req.Greeting.FirstName + req.Greeting.LastName}, nil
+}
+
+// server streaming
+func (*server) GreetManyTimes(req *greetpb.GreetManyTimesRequest, stream greetpb.GreetService_GreetManyTimesServer) error {
+	fmt.Printf("GreetManyTimes function was invoked with %v \n", req)
+
+	for i := 0; i < 10; i++ {
+		result := "Hello " + req.Greeting.FirstName + req.Greeting.LastName + " number" + strconv.Itoa(i)
+		res := &greetpb.GreetManyTimesResponse{
+			Result: result,
+		}
+		stream.Send(res)
+		time.Sleep(1000 * time.Millisecond)
+	}
+	return nil
+}
+
+// client streaming
+func (*server) LongGreet(stream greetpb.GreetService_LongGreetServer) error {
+	fmt.Printf("GreetManyTimes function was invoked with a client streaming request \n")
+
+	result := ""
+
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			stream.SendAndClose(&greetpb.LongGreetResponse{
+				Result: result,
+			})
+			return nil
+		}
+		if err != nil {
+			log.Fatalf("Error while reading client stream: %v", err)
+		}
+
+		firstName := req.Greeting.FirstName
+		result += "Hello " + firstName + "! "
+		log.Print(result)
+	}
+}
+
+// bidirectional streaming
+func (*server) GreetEveryone(stream greetpb.GreetService_GreetEveryoneServer) error {
+	fmt.Printf("GreetEveryone function was invoked \n")
+	for {
+		req, err := stream.Recv()
+		if err != nil {
+			if err == io.EOF {
+				return nil
+			}
+			log.Fatalf("Errorr while reading client stream: %v", err)
+			return err
+		}
+		firstName := req.Greeting.FirstName
+		result := "Hello " + firstName + "! "
+		err = stream.Send(&greetpb.GreetEveryoneResponse{
+			Result: result,
+		})
+
+		if err != nil {
+			log.Fatalf("Errorr while sending data to client: %v", err)
+			return err
+		}
+
+	}
 }
 
 func (*server) mustEmbedUnimplementedGreetServiceServer() {}
@@ -27,6 +95,7 @@ func main() {
 	if err != nil {
 		log.Fatalf("cannot start listening")
 	}
+	log.Print("Server is listening! \n")
 
 	s := grpc.NewServer()
 	greetpb.RegisterGreetServiceServer(s, &server{})
